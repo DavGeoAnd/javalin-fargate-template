dafd198cc5f842b0a2846f09eeedafa2229051d7
David G Andalcio <davega9@gmail.com>
2023-09-05 19:28:58 -0500

    Github Action - add availability.zone to property

--- src/main/java/com/davgeoand/api/controller/AdminController.java
+++ src/main/java/com/davgeoand/api/controller/AdminController.java
@@ -11,6 +11,7 @@ import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,7 +25,6 @@ public class AdminController {
         return () -> {
             get("metrics", AdminController::metrics);
             get("info", AdminController::info);
-            get("aws", AdminController::aws);
         };
     }
 
@@ -42,19 +42,5 @@ public class AdminController {
         context.json(ServiceProperties.getInfoPropertiesMap());
         context.status(HttpStatus.OK);
         log.info("Finished admin info request");
-    }
-
-    private static void aws(Context context) throws IOException {
-        log.info("Starting admin aws request");
-        String ecsMetadataUrl = System.getenv("ECS_CONTAINER_METADATA_URI_V4") + "/task";
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(ecsMetadataUrl)
-                .build();
-        Call call = client.newCall(request);
-        Response response = call.execute();
-        context.json(response.body().string());
-        context.status(HttpStatus.OK);
-        log.info("Finished admin aws request");
     }
 }
